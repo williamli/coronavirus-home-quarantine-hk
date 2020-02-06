c3411cc8b350c72cb862c2094c42f288e0bb685b
William Li <williamli@bbi.io>
2020-02-06 19:27:23 +0800

    renamed project

--- components/nav.js
+++ components/nav.js
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 const links = [
   // { href: 'https://zeit.co/now', label: 'ZEIT' },
-  { href: 'https://github.com/williamli/coronavirus-home-quarantine-hk', label: 'GitHub' },
+  { href: 'https://github.com/williamli/coronavirus-hk-dashboard', label: 'GitHub' },
 ].map(link => {
   link.key = `nav-link-${link.href}-${link.label}`;
   return link;
