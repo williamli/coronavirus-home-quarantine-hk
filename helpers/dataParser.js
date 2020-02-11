96b3210806aa0bf9b1c1b2d996c713f94ccd3bc5
William Li <williamli@bbi.io>
2020-02-11 18:15:45 +0800

    dataParser EN supports

--- helpers/dataParser.js
+++ helpers/dataParser.js
@@ -9,6 +9,35 @@ String.prototype.removeNumbers = function () {
   return this.replace(new RegExp(/^\d+\. /, 'gm'), '.');
 }
 
+String.prototype.tokenizeRecords = function () {
+  return this.split(/^\./gm).slice(1);
+}
+
+String.prototype.formatTimestamp = function () {
+  
+  return this.replace(/^(.*)(\d\d+\/\d+\/\d\d\d\d)(.*)/, "$1 $3, $2");
+}
+
+String.prototype.removeLineBreaks = function () {
+  
+  return this.replace(/(\r\n|\n|\r)/gm, " ");
+}
+
+String.prototype.cleanupSpaces = function () {
+  
+  return this.replace(/  +/gm, ' ').replace(/ ,/gm, ',');
+}
+
+String.prototype.formatAddress = function (locale) {
+  const formatter = new RegExp(`^(${getDistricts(locale)})(\\s+)(.*)(\\,\\s\\d\\d+\\\/\\d+\\\/\\d\\d\\d\\d)$`, "m")
+  
+  return this.replace(formatter, "\"$1\",\"$3\"$4");
+}
+
+const getDistricts = (locale) => {
+  return locale === 'en-HK' ? "Central & Western|Eastern|Southern|Wan Chai|Sham Shui Po|Kowloon City|Kwun Tong|Wong Tai Sin|Yau Tsim Mong|Islands|Kwai Tsing|North|Sai Kung|Sai Hung|Sha Tin|Tai Po|Tsuen Wan|Tuen Mun|Yuen Long" : /Test/;
+}
+
 const getHeadings = (locale) => {
   return locale === 'en-HK' ? `District Name of Building End Date of Home
 Quarantine` : `地區 大廈名稱 家居檢疫最後日期`;
@@ -16,9 +45,13 @@ Quarantine` : `地區 大廈名稱 家居檢疫最後日期`;
 
 const rawToCSV = (rawContent, locale) => {
 
-  
+  const records = rawContent.removeHeadings(locale).removeNumbers().tokenizeRecords().map((record, i)=>{
 
-  return rawContent.removeHeadings(locale).removeNumbers();
+    return record.removeLineBreaks().formatTimestamp().cleanupSpaces().formatAddress(locale);
+
+  })
+
+  return records.join("\n\n");
 
 }
 
